3e8a0f875b494efaca68aa5e4c9c438bffc42ba5
0livia8899 <0livia_hu@aefinder.io>
2024-06-05 23:35:15 +0800

    fix: 🐛 change address

--- src/constant/testnet.ts
+++ src/constant/testnet.ts
@@ -1,3 +1,3 @@
 // testnet-dev
-export const AeFinderHost = 'http://192.168.71.128:8081';
-export const AeFinderAuthHost = 'http://192.168.71.128:8082';
+export const AeFinderHost = '';
+export const AeFinderAuthHost = '';
