ad305acbd6fd19a1edb25a4212be24623d866efa
0livia8899 <0livia_hu@aefinder.io>
2024-06-08 21:50:12 +0800

    feat: 🎸 add mainnet host

--- src/constant/mainnet.ts
+++ src/constant/mainnet.ts
@@ -1,3 +1,3 @@
 // TODO mainnet change the address of the aefinder server
-export const AeFinderHost = 'https://gcp-indexer-api.aelf.io';
+export const AeFinderHost = 'https://indexer-api.aefinder.io';
 export const AeFinderAuthHost = 'https://indexer-auth.aefinder.io';
